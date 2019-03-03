e6c1ed23f1c96bf6572e854b7c4e646b6406e12c
David Vanderburgh <david.a.vanderburgh@gmail.com>
2019-03-03 13:13:42 -0500

    CorruptionChecksum initial commit

---
+++ CorruptionChecksum/CorruptionChecksum/CorruptionChecksumLogic.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorruptionChecksum
+{
+    public static class CorruptionChecksumLogic
+    {
+        public static int GetChecksum(int[][] worksheet)
+        {
+            int checksum = 0;
+            for (int i = 0; i < worksheet.Length; i++)
+            {
+                checksum += GetRowMinMaxDifference(worksheet[i]);
+            }
+            return checksum;
+        }
+
+        public static int GetRowMinMaxDifference(int[] inputArray)
+        {
+            return GetMaxInArray(inputArray) - GetMinInArray(inputArray);
+        }
+
+        public static int GetMaxInArray(int[] inputArray)
+        {
+            int max = inputArray[0];
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] > max)
+                {
+                    max = inputArray[i];
+                }
+            }
+            return max;
+        }
+
+        public static int GetMinInArray(int[] inputArray)
+        {
+            int min = inputArray[0];
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] < min)
+                {
+                    min = inputArray[i];
+                }
+            }
+            return min;
+        }
+
+        public static string GetJaggedArrayString(int[][] jaggedArray)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    result.Append(jaggedArray[i][j] + " ");
+                }
+                result.Append("\n");
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ CorruptionChecksum/CorruptionChecksum/UserInterface.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorruptionChecksum
+{
+    public class UserInterface
+    {
+        public void Run()
+        {
+            int[][] testArray1 =
+{
+                new [] {5, 1, 9, 5},
+                new [] {7, 5, 3},
+                new [] {2, 4, 6, 8}
+            };
+
+            int[][] testArray2 =
+            {
+                new [] {10, 10},
+                new [] {7, -5, 0},
+                new [] {8},
+                new [] {0}
+            };
+
+            int[][] testArray3 =
+            {
+                new [] {6, 3, 18, 24 }
+            };
+
+            Console.WriteLine(CorruptionChecksumLogic.GetChecksum(testArray1));
+            Console.WriteLine(CorruptionChecksumLogic.GetChecksum(testArray2));
+            Console.WriteLine(CorruptionChecksumLogic.GetChecksum(testArray3));
+
+            Console.ReadLine();
+        }
+    }
+}
