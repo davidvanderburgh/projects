e788aa5c7fab6fc4345d159b2ae5c7f668f1397f
David Vanderburgh <david.a.vanderburgh@gmail.com>
2019-03-03 20:00:07 -0500

    Initial commit

---
+++ SpiralMemory/SpiralMemory/UserInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpiralMemory
+{
+    public class UserInterface
+    {
+        public void Run()
+        {
+            SpiralMemoryLogic spiralMemoryLogic = new SpiralMemoryLogic();
+            Console.WriteLine(spiralMemoryLogic.GetNumberOfSteps(1024));
+            Console.ReadLine();
+        }
+    }
+}
