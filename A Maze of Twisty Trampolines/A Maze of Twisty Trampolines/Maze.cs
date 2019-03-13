a9c4c99174cd629fffa6bfce74830cb1c837dfb6
David Vanderburgh <david.a.vanderburgh@gmail.com>
2019-03-12 21:02:42 -0400

    forming classes

--- A Maze of Twisty Trampolines/A Maze of Twisty Trampolines/Maze.cs
+++ A Maze of Twisty Trampolines/A Maze of Twisty Trampolines/Maze.cs
@@ -14,18 +14,21 @@ namespace A_Maze_of_Twisty_Trampolines
             int[] workingStepArray = stepArray;
 
             //check that the current index is within the array, if not then the loop ends
-            // set the nextIndex to the currentIndex plus (the value at the currentIndex)
-            // increment the value at the currentIndex by 1
-            // add one to the number of steps
-            // set the currentIndex to the nextIndex
-            // loop around
-
             while (currentIndex >= 0 && currentIndex < workingStepArray.Length)
             {
+                //set the nextIndex to the currentIndex plus the value at the currentIndex
                 nextIndex = currentIndex + workingStepArray[currentIndex];
+
+                //increment the value at the currentIndex by 1
                 workingStepArray[currentIndex]++;
+
+                //add one to the number of steps
                 steps++;
+
+                //set the currentIndex to the nextIndex
                 currentIndex = nextIndex;
+                
+                //loop around
             }
 
             return steps;
